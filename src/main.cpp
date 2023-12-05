7fd1eb571b1315048da3fc02271dc7d0447ce66f
Ian Brown <bigianb@gmail.com>
2023-12-05 16:58:29 +0000

    load the modules

--- src/main.cpp
+++ src/main.cpp
@@ -2,20 +2,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <libcdvd.h>
+#include <loadfile.h>
 #include <sifrpc.h>
 
 #include "memcard_test.h"
 
 int main()
 {
-	printf("Hello\n");
-	SifInitRpc(0);
-	sceCdInit(SCECdINIT);
-	
-	auto memcardTest = MemcardTest();
-	memcardTest.run();
+    printf("Hello\n");
+    SifInitRpc(0);
+    sceCdInit(SCECdINIT);
 
-	printf("That's all folks\n");
+    int sifLoadRet = SifLoadModule("rom0:SIO2MAN", 0, NULL);
+	if (sifLoadRet < 0) {
+		printf("Failed to load module: SIO2MAN");
+        return -1;
+	}
 
-	return 0;
+	sifLoadRet = SifLoadModule("rom0:MCMAN", 0, NULL);
+	if (sifLoadRet < 0) {
+		printf("Failed to load module: MCMAN");
+        return -1;
+	}
+
+	sifLoadRet = SifLoadModule("rom0:MCSERV", 0, NULL);
+	if (sifLoadRet < 0) {
+		printf("Failed to load module: MCSERV");
+        return -1;
+	}
+
+    auto memcardTest = MemcardTest();
+    memcardTest.run();
+
+    printf("That's all folks\n");
+
+    return 0;
 }
--- src/memcard_test.cpp
+++ src/memcard_test.cpp
@@ -7,24 +7,25 @@
 #include <sifrpc.h>
 #include <libmc.h>
 
-
 bool MemcardTest::init()
 {
     printf("Initialising memcard\n");
     int rval = mcInit(MC_TYPE_MC);
-    printf("mcInit returned %d\n",rval);
-    if( rval < 0) {
-		printf("Failed to initialise memcard server!\n");
-		return false;
-	}
+    printf("mcInit returned %d\n", rval);
+    if (rval < 0)
+    {
+        printf("Failed to initialise memcard server!\n");
+        return false;
+    }
     return true;
 }
 
 bool MemcardTest::run()
 {
     printf("Memcard test\n");
-    
-    if (!init()){
+
+    if (!init())
+    {
         printf("Init failed\n");
         return false;
     }
@@ -39,59 +40,64 @@ bool MemcardTest::run()
     int port = 0;
     int slot = 0;
 
-	int rval = mcGetInfo(port, slot, &mc_Type, &mc_Free, &mc_Format);
-    printf("mcGetInfo returned %d\n",rval);
-	mcSync(0, NULL, &ret);
-	printf("mcGetInfo sync returned %d\n",ret);
-	printf("Type: %d Free: %d Format: %d\n", mc_Type, mc_Free, mc_Format);
-    if (ret != -1){
+    int rval = mcGetInfo(port, slot, &mc_Type, &mc_Free, &mc_Format);
+    printf("mcGetInfo returned %d\n", rval);
+    mcSync(0, NULL, &ret);
+    printf("mcGetInfo sync returned %d\n", ret);
+    printf("Type: %d Free: %d Format: %d\n", mc_Type, mc_Free, mc_Format);
+    if (ret != -1)
+    {
         success = false;
     }
 
-	rval = mcGetInfo(port, slot, &mc_Type,&mc_Free,&mc_Format);
-    printf("mcGetInfo returned %d\n",rval);
-	mcSync(0, NULL, &ret);
-	printf("mcGetInfo sync returned %d\n",ret);
-	printf("Type: %d Free: %d Format: %d\n", mc_Type, mc_Free, mc_Format);
-    if (ret != 0){
+    rval = mcGetInfo(port, slot, &mc_Type, &mc_Free, &mc_Format);
+    printf("mcGetInfo returned %d\n", rval);
+    mcSync(0, NULL, &ret);
+    printf("mcGetInfo sync returned %d\n", ret);
+    printf("Type: %d Free: %d Format: %d\n", mc_Type, mc_Free, mc_Format);
+    if (ret != 0)
+    {
         success = false;
     }
 
-    const int ARRAY_ENTRIES	= 64;
+    const int ARRAY_ENTRIES = 64;
     sceMcTblGetDir mcDir[ARRAY_ENTRIES] __attribute__((aligned(64)));
     mcGetDir(0, 0, "/*", 0, ARRAY_ENTRIES, mcDir);
     int numEntries;
-	mcSync(0, NULL, &numEntries);
-	printf("mcGetDir returned %d\n\nListing of root directory on memory card:\n\n", ret);
+    mcSync(0, NULL, &numEntries);
+    printf("mcGetDir returned %d\n\nListing of root directory on memory card:\n\n", ret);
 
-    const char * ourDirName = "FUNCTEST";
+    const char *ourDirName = "FUNCTEST";
     bool foundOurDir = false;
 
-    for(int i=0; i < numEntries; i++)
-	{
-		if((mcDir[i].AttrFile & MC_ATTR_SUBDIR) == MC_ATTR_SUBDIR) {
-			printf("[DIR] %s\n", mcDir[i].EntryName);
-            if (0 == strcmp((char *)mcDir[i].EntryName, ourDirName)){
+    for (int i = 0; i < numEntries; i++)
+    {
+        if ((mcDir[i].AttrFile & MC_ATTR_SUBDIR) == MC_ATTR_SUBDIR)
+        {
+            printf("[DIR] %s\n", mcDir[i].EntryName);
+            if (0 == strcmp((char *)mcDir[i].EntryName, ourDirName))
+            {
                 foundOurDir = true;
-
             }
-        } else {
-			printf("%s - %d bytes\n", mcDir[i].EntryName, mcDir[i].FileSizeByte);
         }
-	}
+        else
+        {
+            printf("%s - %d bytes\n", mcDir[i].EntryName, mcDir[i].FileSizeByte);
+        }
+    }
 
-    if (foundOurDir){
+    if (foundOurDir)
+    {
         rval = mcDelete(port, slot, ourDirName);
-        printf("mcDelete returned %d\n",rval);
+        printf("mcDelete returned %d\n", rval);
         mcSync(0, NULL, &ret);
         printf("mcDelete sync returned %d\n", ret);
     }
 
     rval = mcMkDir(port, slot, ourDirName);
-    printf("mcMkDir returned %d\n",rval);
+    printf("mcMkDir returned %d\n", rval);
     mcSync(0, NULL, &ret);
     printf("mcMkDir sync returned %d\n", ret);
-
 
     char cdir[256];
     rval = mcChdir(port, slot, ourDirName, cdir);
